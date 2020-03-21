d6c8e9c112ac95a5ced3f65bdea7d6524bbe1d1e
JanviRaina <50794649+JanviRaina@users.noreply.github.com>
2020-03-22 00:13:12 +0530

    Added main python file

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,request
+import pickle
+
+app = Flask(__name__)
+
+
+file=open('model.pkl','rb')
+clf=pickle.load(file)
+file.close()
+
+@app.route('/',methods=['GET','POST'])
+def hello_world():
+    if request.method=='POST': 
+        myDict=request.form
+        fever=int(myDict['fever'])
+        age=int(myDict['age'])
+        pain=int(myDict['pain'])
+        runnyNose=int(myDict['runnyNose'])
+        diffBreathe=int(myDict['diffbreath'])
+
+
+        inputFeatures=[fever,pain,age,runnyNose,diffBreathe]
+        infProb=clf.predict_proba([inputFeatures])[0][1]
+        print(infProb)
+
+        return render_template('show.html',inf=round(infProb*100))
+    return render_template('index.html')
+    # return "hello world"+ str(infProb)
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
